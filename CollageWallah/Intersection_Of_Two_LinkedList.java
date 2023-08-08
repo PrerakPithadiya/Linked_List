07c079255fb09f36183ab48920f6a0445226480e
PrerakPithadiya <139850989+PrerakPithadiya@users.noreply.github.com>
2023-08-09 00:43:39 +0530

    Create Intersection_Of_Two_LinkedList.java

---
+++ CollageWallah/Intersection_Of_Two_LinkedList.java
@@ -0,0 +1,39 @@
+public class Intersection_Of_Two_LinkedList{
+  public ListNode getIntersectionNode(ListNode headA, ListNode headB){
+        ListNode tempA = headA;
+        ListNode tempB = headB;
+
+        // get length of list 1
+        int m = 0;
+        for(; tempA != null; tempA = tempA.next){
+            m++;
+        }
+
+        // get length of list 2
+        int n = 0;
+        for(; tempB != null; tempB = tempB.next){
+            n++;
+        }
+
+        tempA = headA;
+        tempB = headB;
+
+        if(m > n){
+            int steps = m - n;
+            for(int i = 1; i <= steps; i++){
+                tempA = tempA.next;
+            }
+        } else{
+            int steps = n - m;
+            for(int i = 1; i <= steps; i++){
+                tempB = tempB.next;
+            }
+        }
+
+        while(tempA != tempB){
+            tempA = tempA.next;
+            tempB = tempB.next;
+        }
+
+        return tempA;
+}
